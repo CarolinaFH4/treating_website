73f4c52d77c33107037eba3341fc1733c33f54d8
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-02 15:32:28 +0100

    Header and first main section added

--- index.php
+++ index.php
@@ -6,8 +6,61 @@
     <title>Treating Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
+
+
+
   <body>
-    <h1>Aqui vai nascer o meu projeto!</h1>
+    <header>
+      <div>
+        <div>
+          <h1>
+            Treating
+          </h1>
+        </div>
+        <h2>
+         Onde a nutrição e a comunidade se encontram
+        </h2>
+      </div>
+    </header>
+
+
+    <main>
+      <div>
+        <section id="introcomunity">
+          <div>
+            <img src="" alt="people eating together">
+          </div>
+          <a href="">Comunidade</a>
+          <p>
+            Treating vem esclarecer conceitos de nutrição e juntar pessoas através do poder da alimentação
+          </p>
+        </section>
+        <section id="introrecipes">
+          <div>
+            <img src="" alt="various organic dishes">
+          </div>
+          <a href="">Receitas</a>
+          <p>
+            Selecionamos os melhores alimentos para tranformar nas melhores receitas.
+          </p>
+        </section>
+        <section id="introfood">
+          <div>
+            <img src="" alt="body parts representation">
+          </div>
+          <a href="">Benefícios de saúde</a>
+          <p>
+            Olhamos para os alimentos como ferramentas de auto-cuidado e terapia, em vez de tendências e restrições.
+          </p>
+        </section>
+      </div>
+    </main>
+
+
+    <footer>
+
+    </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
