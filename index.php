fe3466662d3a19d2e865d6aa41ec5e62ea629f57
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-02 23:13:08 +0100

    CSS no index

--- index.php
+++ index.php
@@ -5,15 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Treating Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./CC/styles.css">
+    <link rel="stylesheet" href="./CSS/styles.css">
   </head>
 
 
 
   <body>
+
     <header>
       <div>
-        <div>
+        <div class="logoborder">
           <h1>
             Treating
           </h1>
@@ -24,12 +25,11 @@
       </div>
     </header>
 
-
     <main>
       <section class="introduction">
         <div>
           <div>
-            <div>
+            <div class="introimg">
               <img src="./media/images/comunity.png" alt="people eating together">
             </div>
             <a href="">Comunidade</a>
@@ -38,7 +38,7 @@
             </p>
           </div>
           <div>
-            <div>
+            <div class="introimg">
               <img src="./media/images/recipes.png" alt="various organic dishes">
             </div>
             <a href="">Receitas</a>
@@ -47,7 +47,7 @@
             </p>
           </div>
           <div>
-            <div>
+            <div class="introimg">
               <img src="./media/images/organs.png" alt="body parts representation">
             </div>
             <a href="">Benefícios de saúde</a>
@@ -76,11 +76,15 @@
       </section>
     </main>
 
-
     <footer>
-
+      <div>
+      <svg id="wave" style="transform:rotate(0deg); transition: 0.3s" viewBox="0 0 1440 340" version="1.1" xmlns="http://www.w3.org/2000/svg"><path style="transform:translate(0, 0px); opacity:1" fill="#229b1e" 
+      d="M0,34L40,45.3C80,57,160,79,240,102C320,125,400,147,480,141.7C560,136,640,102,720,119C800,136,880,204,960,215.3C1040,227,1120,181,1200,136C1280,91,1360,45,1440,28.3C1520,11,1600,23,1680,34C1760,45,1840,57,1920,73.7C2000,91,2080,113,2160,153C2240,193,2320,249,2400,272C2480,295,2560,283,2640,272C2720,261,2800,249,2880,238C2960,227,3040,215,3120,204C3200,193,3280,181,3360,153C3440,125,3520,79,3600,68C3680,57,3760,79,3840,113.3C3920,147,4000,193,4080,187C4160,181,4240,125,4320,119C4400,113,4480,159,4560,147.3C4640,136,4720,68,4800,62.3C4880,57,4960,113,5040,124.7C5120,136,5200,102,5280,79.3C5360,57,5440,45,5520,85C5600,125,5680,215,5720,260.7L5760,306L5760,340L5720,340C5680,340,5600,340,5520,340C5440,340,5360,340,5280,340C5200,340,5120,340,5040,340C4960,340,4880,340,4800,340C4720,340,4640,340,4560,340C4480,340,4400,340,4320,340C4240,340,4160,340,4080,340C4000,340,3920,340,3840,340C3760,340,3680,340,3600,340C3520,340,3440,340,3360,340C3280,340,3200,340,3120,340C3040,340,2960,340,2880,340C2800,340,2720,340,2640,340C2560,340,2480,340,2400,340C2320,340,2240,340,2160,340C2080,340,2000,340,1920,340C1840,340,1760,340,1680,340C1600,340,1520,340,1440,340C1360,340,1280,340,1200,340C1120,340,1040,340,960,340C880,340,800,340,720,340C640,340,560,340,480,340C400,340,320,340,240,340C160,340,80,340,40,340L0,340Z"></path></svg>
+      </div>
     </footer>
 
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
   </body>
 </html>
