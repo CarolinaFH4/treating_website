4bdd552113d3ec3e01805a4724bc2758fae2afb8
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-02 19:20:50 +0100

    Tentativa de usar svg e preencher com img

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Treating Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./CC/styles.css">
   </head>
 
 
@@ -26,9 +27,23 @@
 
     <main>
       <div>
+
+      <section id="introcomunity">
+        <div class="blob">
+          <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <pattern id="imgpattern" patternUnits="objectBoundingBox" width="1" height="1">
+                <image href="./media/images/comunity.jpg" x="0" y="0" preserveAspectRatio="xMidYMid slice" />
+              </pattern>
+            </defs>
+            <path d="M29,-34.1C41.2,-31.1,57.2,-27.4,68.7,-16.6C80.2,-5.7,87.2,12.5,81.2,25C75.2,37.5,56.3,44.5,40.6,46.3C24.9,48.1,12.5,44.7,0.4,44.2C-11.7,43.6,-23.3,45.9,-34,42.5C-44.6,39,-54.3,29.9,-54.3,20C-54.3,10.1,-44.6,-0.7,-38.6,-10.4C-32.5,-20.2,-30,-29,-24.2,-34.1C-18.3,-39.3,-9.2,-40.7,-0.4,-40.1C8.4,-39.6,16.8,-37.2,29,-34.1Z" transform="translate(100 100)" fill="url(#imgpattern)"/>
+          </svg>
+        </div>
+      </section>
+
         <section id="introcomunity">
-          <div>
-            <img src="" alt="people eating together">
+          <div class="blob">
+            <img src="./media/images" alt="people eating together">
           </div>
           <a href="">Comunidade</a>
           <p>
