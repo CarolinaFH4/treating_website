38bf47cb3638cb6275d17d66659e7e0bc394edb1
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-05 09:50:43 +0100

    Changing logo.svg

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 
 <header>
   <div class= "row justify-content-center">
-    <div class= "col-auto">
+    <div class= "col-auto justify-content-center">
         <a href="index.php" class="col">
-          <img src="./media/icons/treating_logo.svg" alt="Treating logo">
+          <img src="./media/icons/logo.svg" alt="Treating logo">
         </a>
     </div>
   </div>
