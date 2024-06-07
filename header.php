c4a0ccd59b5cc9c44f5c923d37d5d9f174deac09
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-07 17:38:53 +0100

    categorias do menu

--- header.php
+++ header.php
@@ -33,17 +33,21 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-
+          
           <div class="collapse navbar-collapse" id="navbarNav">
+
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="#">Início</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Alimentos</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link" href="#">Comunidade</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-disabled="true">Receitas</a>
               </li>
             </ul>
           </div>
