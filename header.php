dbb2dcbea6afbf1bc501adfc4141e72ff61ab361
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-07 17:27:00 +0100

    cleaning logo code

--- header.php
+++ header.php
@@ -15,9 +15,9 @@
   <body>
     <section>
       <div class="container">
-        <div class= "row justify-content-center ">
+        <div class= "d-flex justify-content-center ">
           <div class= "col-auto m-4">
-              <a href="index.php" class="col">
+              <a href="index.php">
                 <img src="./media/icons/logo.svg" alt="Treating logo" >
               </a>
           </div>
@@ -26,12 +26,14 @@
     </section>
 
     <section> 
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar navbar-expand-sm bg-body-tertiary">
         
         <div class="container-fluid">
+
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
@@ -43,11 +45,9 @@
               <li class="nav-item">
                 <a class="nav-link" href="#">Pricing</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
             </ul>
           </div>
+
         </div>
       </nav>
     </section>
