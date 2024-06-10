cc108d1ce9dd29049c18144ab2f094761c24d696
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-10 15:51:12 +0100

    limpeza de header

--- header.php
+++ header.php
@@ -1,17 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@1,700&family=Heebo&family=Spinnaker&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./CSS/styles.css">
-  </head>
 
-
-  <body class="">
     <header>
       <div>
         <section>
@@ -51,10 +38,3 @@
         </section>
       </div>
     </header>
-
-
-
-
-    
-  </body>
-</html>
