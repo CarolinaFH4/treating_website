0fb84a0c4471d76787d3ae6f481abf47e66adfd9
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-25 10:59:22 +0100

    db update and tests with glossary

--- detalhealimento.php
+++ detalhealimento.php
@@ -54,6 +54,11 @@
               WHERE idfood = $id";
   $rresult = mysqli_query($connection, $queryr);
 
+  $queryglo = "SELECT word, description, parameter, category
+                FROM glossary
+                WHERE word = "; 
+  $gresult = mysqli_query($connection, $queryglo);
+
 ?>
 
 
@@ -160,7 +165,22 @@
           </table>
         </div>
       </section>
+
+      <section class="test">
+        <table class="table">
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td><a href="#" data-bs-toggle="tooltip" data-bs-title="default tooltip">Mark</a></td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
       
+      <p class="muted">Placeholder text to demonstrate some <a href="#" data-bs-toggle="tooltip" data-bs-title="Default tooltip">inline links</a> with tooltips. This is now just filler, no killer. Content placed here just to mimic the presence of <a href="#" data-bs-toggle="tooltip" data-bs-title="Another tooltip">real text</a>. And all that just to give you an idea of how tooltips would look when used in real-world situations. So hopefully you've now seen how <a href="#" data-bs-toggle="tooltip" data-bs-title="Another one here too">these tooltips on links</a> can work in practice, once you use them on <a href="#" data-bs-toggle="tooltip" data-bs-title="The last tip!">your own</a> site or project.</p>
+
       <section> 
         <div id="carouselExampleCaptions" class="carousel slide">
           <div class="carousel-indicators">
