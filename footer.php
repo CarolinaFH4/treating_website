b641de5828f514dcb26255bb830de77a124fb84d
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-27 23:23:39 +0100

    detalhes footer

--- footer.php
+++ footer.php
@@ -3,28 +3,29 @@
       <img class="wavef" src="./media/icons/wave.footer.svg" alt="wave separation">
   </div>
   
-  <div class="d-flex flex-column align-items-center bg-secondary-subtle">
-    <a href="index.php" class="mb-4">
-      <img src="./media/icons/logo_no_border.svg" alt="Treating logo">
-    </a>
-    <div class="d-flex justify-content-between">
-      <ul class="linksf">
-        <li>
-          <a href="">Contacto</a>
-        </li>
-        <li>
-          <a href="">Biblioteca</a>
-        </li>
-        <li>
-          <a href="">Política de privacidade</a>
-        </li>
-      </ul>
+  <div class="footer container d-flex flex-column bg-secondary-subtle">
 
-      <div  class="">
-        <img type="link" href="" src="./media/icons/instagram.svg" alt="Instagram">
-        <img type="link" href="" src="./media/icons/pinterest.svg" alt="Pinterest">
-        <img type="link" href="" src="./media/icons/linkedin.svg" alt="LinkedIn">
-        <img type="link" href="" src="./media/icons/youtube.svg" alt="Youtube">
+    <div class="mb-3 align-self-center">
+      <a href="index.php">
+        <img src="./media/icons/logo_no_border.svg" alt="Treating logo">
+      </a>
+    </div>
+
+    <div class="d-flex justify-content-between">
+      <div class="d-flex flex-column">
+            <a href="">Contacto</a>
+            <a href="">Biblioteca</a>
+            <a href="">Política de privacidade</a>
+      </div>
+      <div class="d-flex flex-column gap-2">
+        <div class="d-flex gap-2">
+          <img class="" type="link" href="" src="./media/icons/instagram.svg" alt="Instagram">
+          <img class="" type="link" href="" src="./media/icons/pinterest.svg" alt="Pinterest">
+        </div>
+        <div class="d-flex gap-2">
+          <img class="" type="link" href="" src="./media/icons/linkedin.svg" alt="LinkedIn">
+          <img class="" type="link" href="" src="./media/icons/youtube.svg" alt="Youtube">
+        </div>
       </div>
     </div>
   </div>
