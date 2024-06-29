51c9bc7d03cecdf32a5d790133b476418cbbdbe0
CarolinaFH4 <tristeplantart@gmail.com>
2024-06-30 00:42:01 +0100

    adding modal for form

--- index.php
+++ index.php
@@ -75,8 +75,31 @@
           </section>
 
           <div class="mt-4 text-center">
-            <h3>O teu bem estar e esta plataforma são trabalhos em constante evolução. 
-              <a href="" alt="contact form" class="text-reset">Partilha connosco</a> as tuas preocupações e temas que queres ver abordados!</h3>
+            <h3>
+              O teu bem estar e esta plataforma são trabalhos em constante evolução. 
+              <a type="" class="" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Partilha connosco
+              </a> 
+              as tuas preocupações e temas que queres ver abordados!
+            </h3>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
           </div>
         </div>
     </main>
